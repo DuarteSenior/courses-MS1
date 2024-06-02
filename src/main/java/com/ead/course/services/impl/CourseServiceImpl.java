2af7949e4e950134cc83ac47a0d072a2da23cd81
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-06-01 21:08:03 -0300

    service de Lesson, Module e Course mapeados

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,6 +1,14 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.reposiories.CourseRepository;
 import com.ead.course.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    CourseRepository courseRepository;
+
 }
