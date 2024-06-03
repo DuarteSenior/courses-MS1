5b882780446bf67ec4099b727229a04d2df9bf46
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-06-03 16:17:42 -0300

    inclusao do cascade, caso delete uma entidade, deleta também todas as entidades relacionadas a ela

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,9 +1,16 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.reposiories.CourseRepository;
+import com.ead.course.reposiories.LessonRepository;
+import com.ead.course.reposiories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
@@ -11,4 +18,22 @@ public class CourseServiceImpl implements CourseService {
     @Autowired
     CourseRepository courseRepository;
 
+    @Autowired
+    ModuleRepository moduleRepository;
+
+    @Autowired
+    LessonRepository lessonRepository;
+
+    @Override
+    public void delete(CourseModel courseModel) {
+        List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+        if (!moduleModelList.isEmpty()) {
+            for (ModuleModel module : moduleModelList) {
+                List<LessonModel> lessonModelList = lessonRepository.findAllLesonsIntoModule(module.getModuleId());
+                if (!lessonModelList.isEmpty()) {
+                    lessonRepository.deleteAll(lessonModelList);
+                }
+            }
+        }
+    }
 }
