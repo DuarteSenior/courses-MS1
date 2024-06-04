52f6f39682884fec7231951734aba0fb0cecf169
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-06-04 18:29:08 -0300

    rota de criação de course criada

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -1,9 +1,16 @@
 package com.ead.course.controllers;
 
+import com.ead.course.dtos.CourseDto;
+import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @RequestMapping("/courses")
@@ -14,7 +21,13 @@ public class CourseController {
     CourseService courseService;
 
     @PostMapping
-    public ResponseEntity<Object> saveCourse(@ResponseBody CourseDto courseDto){
+    public ResponseEntity<Object> saveCourse(@RequestBody CourseDto courseDto) {
+        var courseModel = new CourseModel();
+        BeanUtils.copyProperties(courseDto, courseModel);
+        courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseService.save(courseModel);
 
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseModel););
     }
 }
