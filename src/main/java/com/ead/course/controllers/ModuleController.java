270b8aa4b484b29e8511d438ea3c7e1f8e608a59
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-09-22 15:34:21 -0300

    ModuleDto criado

--- src/main/java/com/ead/course/controllers/ModuleController.java
+++ src/main/java/com/ead/course/controllers/ModuleController.java
@@ -1,9 +1,21 @@
 package com.ead.course.controllers;
 
+import com.ead.course.dtos.CourseDto;
+import com.ead.course.dtos.ModuleDto;
+import com.ead.course.models.CourseModel;
 import com.ead.course.services.ModuleService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -11,4 +23,17 @@ public class ModuleController {
 
     @Autowired
     ModuleService moduleService;
+
+    @PostMapping("/courses/{courseId}/modules")
+    public ResponseEntity<Object> saveModule(@RequestBody @Valid ModuleDto moduleDto) {
+        var courseModel = new CourseModel();
+        BeanUtils.copyProperties(courseDto, courseModel);
+        courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseService.save(courseModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseModel));
+    }
+
+
 }
